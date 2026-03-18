0e675d5e4989151c6b1d3997a21ec5e0357e3f41
Thescosp <francesco.petronzio@cenix.com.br>
2026-03-18 20:24:25 -0300

    fix-reminder-notification-frontend

--- preload.js
+++ preload.js
@@ -24,6 +24,8 @@ contextBridge.exposeInMainWorld('buddyAPI', {
   onScheduledTask: (callback) => ipcRenderer.on('scheduled-task', (event, data) => callback(data)),
   onTriggerAgentMessage: (callback) => ipcRenderer.on('trigger-agent-message', (event, data) => callback(data)),
   schedulerTaskDone: () => ipcRenderer.send('scheduler-task-done'),
+  // Reminder events
+  onReminderFired: (callback) => ipcRenderer.on('agent-reminder-fired', (event, data) => callback(data)),
   // Settings
   getSettings: () => ipcRenderer.invoke('get-settings'),
   saveSettings: (settings) => ipcRenderer.invoke('save-settings', settings),
